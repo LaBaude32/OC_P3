604b86fcc23c017110c020abc3f15024551db48f
LaBaude32 <baudouin.coupey@gmail.com>
2019-09-26 20:09:41 +0200

    assemblage des codes. ajout de marqueurs. correction de la requette AJAX

--- main.js
+++ main.js
@@ -39,3 +39,58 @@ function switchDiapo() {
 }
 
 bouton.addEventListener('click', switchDiapo);
+
+
+
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 48.852969;
+var lon = 2.349903;
+var macarte = null;
+// Fonction d'initialisation de la carte
+function initMap(stations) {
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    macarte = L.map('map').setView([lat, lon], 11);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(macarte);
+
+    var customIcon = L.icon({
+        iconUrl: 'icon-marker.png',
+        //shadowUrl: 'icon-shadow.png',
+        iconSize:     [32, 32], // taille de l'icone
+        //shadowSize:   [50, 64], // taille de l'ombre
+        iconAnchor:   [32, 64], // point de l'icone qui correspondra à la position du marker
+        //shadowAnchor: [32, 64],  // idem pour l'ombre
+        popupAnchor:  [-3, -76] // point depuis lequel la popup doit s'ouvrir relativement à l'iconAnchor
+    });
+
+    for (const station of stations) {
+        lat = (station.position.lat);
+        lon = (station.position.lng);
+
+        L.marker([lat, lon], {icon: customIcon}).addTo(macarte);
+    }
+}
+window.onload = function () {
+    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+    // initMap();
+};
+
+
+let api_key = "81aa8312fa8a1075e302560e528cf5d1e0887cea";
+let url = "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=" + api_key;
+
+var stations
+
+let data = fetch(url)
+.then(response => response.json())
+.then(function (data) {
+    stations = data
+
+
+    initMap(stations);
+});
--- requete.js
+++ requete.js
@@ -1,6 +1,11 @@
 let api_key = "81aa8312fa8a1075e302560e528cf5d1e0887cea";
 let url = "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=" + api_key;
 
+var stations
+
 let data = fetch(url)
 .then(response => response.json())
-.then(console.log);
+.then(function (data) {
+    stations = data
+    initMap();
+});
