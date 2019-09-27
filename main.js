3d9d4176e6345e97dc1b89d9e436cf8fc6020678
LaBaude32 <baudouin.coupey@gmail.com>
2019-09-27 17:17:30 +0200

    refacto fonction switchDiapo fini

--- main.js
+++ main.js
@@ -1,52 +1,24 @@
 let bouton = document.querySelector('button');
-let img = document.querySelector('img');
-// img.style.visibility = "visible";
 
 function switchDiapo() {
     let listImgs = document.querySelectorAll('img');
-    let imageAffichee;
-    let nbImages = 0;
-    let css
-    let compteur = 0;
 
-    // img.style.visibility = "visible";/
-    /*
-        for (image of listImgs) {
-            css = getComputedStyle(image).visibility;
-            if (css == "visible") {
-                //recuperer l'image actuelle affichée
-                imageAffichee = nbImages;
-            }
-            nbImages++; //recupere le nombre total d'image
-        }
-
-        for (val of listImgs) {
-            let css = getComputedStyle(val).visibility
-            if (css == "visible") {
-                //cacher les images
-                val.style.visibility = "hidden"
-            }
-            if (compteur == imageAffichee + 1) {
-                // si l'image est celle qui suit l'image affiché, la révéler
-                val.style.visibility = "visible"
-            }
-            compteur++;
-            if (compteur == nbImages - 1) {
-                // si on est à la dernière image, révéler la 1ère
-                img.style.visibility = "visible";
-            }
-        }
-
-    */
-    for (const image in listImgs) {
+    for (const image of listImgs) {
         let val = image.getAttribute('class');
         if (val.includes('active')) {
-            listImgs[image].setAttribute('class', 'imgDiapo img-fluid')
-            console.log(listImgs[image]);
-
+            image.setAttribute('class', 'imgDiapo img-fluid');
+            let valId = image.getAttribute('id');
+            valId = valId.split('-');
+            valId = valId[1];
+            valId++;
+            valId = 'slider-'+ valId;
+            if (valId == 'slider-5'){
+                valId = 'slider-0'
+            }
+            document.getElementById(valId).setAttribute('class', 'imgDiapo img-fluid active')
+            break;
         }
     }
-
 }
 
 bouton.addEventListener('click', switchDiapo);
