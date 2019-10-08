8811376fb27379ca85f40737171f58eef203f56d
LaBaude32 <baudouin.coupey@gmail.com>
2019-10-08 12:26:10 +0200

    beautify

--- Resa.js
+++ Resa.js
@@ -1,43 +1,43 @@
-class Resa {
-    constructor() {
-        this.formStationNom = document.getElementById('stationNom');
-        this.formStationAdresse = document.getElementById('StationAdresse');
-        this.formStationVelosDispos = document.getElementById('StationVelosDispos');
-        this.compteurText = document.getElementById('compteur');
-    }
+class Resa{
+	constructor() {
+		this.formStationNom = document.getElementById('stationNom');
+		this.formStationAdresse = document.getElementById('StationAdresse');
+		this.formStationVelosDispos = document.getElementById('StationVelosDispos');
+		this.compteurText = document.getElementById('compteur');
+	}
 
-    initResa(ID) {
-        let urlStation = "https://api.jcdecaux.com/vls/v3/stations/" + ID + "?contract=Lyon&apiKey=" + api_key;
+	initResa(ID) {
+		let urlStation = "https://api.jcdecaux.com/vls/v3/stations/" + ID + "?contract=Lyon&apiKey=" + api_key;
 
-        let station;
+		let station;
 
-        let data = fetch(urlStation)
-            .then(response => response.json())
-            .then(function (data) {
-                station = data;
+		let data = fetch(urlStation)
+			.then(response => response.json())
+			.then(function (data) {
+				station = data;
 
-                this.formHydrate(station);
-            });
+				this.formHydrate(station);
+			});
 
 
-    }
+	}
 
-    formHydrate(station) {
-        console.log(station);
+	formHydrate(station) {
+		console.log(station);
 
-        // let nom = station.name.split('-')[1];
-        let nom = 'test'
+		// let nom = station.name.split('-')[1];
+		let nom = 'test'
 
-        this.formStationNom.innerText = nom;
-        // formStationAdresse.innerText = stationResa.address;
-        // formStationVelosDispos.innerText = stationResa.mainStands.availabilities.bikes
-    }
+		this.formStationNom.innerText = nom;
+		// formStationAdresse.innerText = stationResa.address;
+		// formStationVelosDispos.innerText = stationResa.mainStands.availabilities.bikes
+	}
 
-    compteur(){
-        console.log(this.compteurText);
+	compteur() {
+		console.log(this.compteurText);
 
-        let dateActu = new Date();
-        // let heures = dateActu.getHours();
-        this.compteurText.innerText = dateActu;
-    }
+		let dateActu = new Date();
+		// let heures = dateActu.getHours();
+		this.compteurText.innerText = dateActu;
+	}
 }
